a54a47a62c3e4fb2eb28cdeb1460d22faec3521b
Vadim1044 <67069732+Vadim1044@users.noreply.github.com>
2025-03-25 20:00:15 +0600

    Add files via upload

---
+++ task_1/add.h
@@ -0,0 +1,5 @@
+int add_int(int a, int b);
+int max(int a, int b);
+int bubbleSort(double arr[], int n);
+void solveQuadratic(double a, double b, double c, double roots[2], int *root_count,
+    double epsilon);
